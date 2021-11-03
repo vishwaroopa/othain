c6a34eac3ddb76ca0c99ff04b11d532487c2ce0a
vishwaroopa <vishwarupa.vishu@gmail.com>
2021-11-03 13:25:35 +0530

    'yty'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ const Sliders = () => {
                                                     <img src={slider.bimage.url} alt="Mavin Infotech" class="item_sub" />
                                                     <img src="https://res.cloudinary.com/mavin-infotech/image/upload/v1629970196/home_banner_5eb28553ba.png?267531901.30000007" alt="Mavin Infotech" class="item_sub1 item_sub2" />
                                                     <div class="carousel-caption">
-                                                        <div className='col-md-7'>
+                                                        <div className='col-md-8'>
                                                             <h1>
                                                                 {slider.title}
                                                             </h1>
